945e82d3eb5371518e6b63c8775102723cfac403
Bigzik10 <niksavage5@gmail.com>
2023-09-18 19:52:56 +0100

    gvhvh

---
+++ src/app/cart/cart-empty/cart-empty.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cart-empty',
+  templateUrl: './cart-empty.component.html',
+  styleUrls: ['./cart-empty.component.css']
+})
+export class CartEmptyComponent {
+
+}
